e71f811ae101d80a9d14c6996bc42c89cd5bebb0
ndgo <durnov.maxim@gmail.com>
2017-06-10 22:30:28 +0300

    refactored js

--- js/app.js
+++ js/app.js
@@ -1,36 +1,36 @@
 function initMap() {
-  var e = {
+  var position = {
     lat: 59.9387942,
     lng: 30.3230833
-  }
-    , t = {
+  };
+  var center = {
     lat: 59.939794,
     lng: 30.3230833
-  }
-    , n = {
+  };
+  var centerParameters = {
     zoom: 15,
-    center: t
-  }
-    , i = new google.maps.Map(document.getElementById("map"), n)
-    , o = {
+    center: center
+  };
+  var map = new google.maps.Map(document.getElementById("map"), centerParameters);
+  var icon = {
     url: "img/icon-map-marker.svg",
     size: new google.maps.Size(100, 100),
     scaledSize: new google.maps.Size(35, 35),
     origin: new google.maps.Point(0, 0),
     anchor: new google.maps.Point(20, 20),
-    optimized: !1,
+    optimized: false,
     zIndex: 1
-  }
-    , a = new google.maps.Marker({
-    position: e,
-    map: i,
-    icon: o,
+  };
+  var marker = new google.maps.Marker({
+    position: position,
+    map: map,
+    icon: icon,
     title: "HTML Academy"
   });
   google.maps.event.addDomListener(window, "resize", function () {
-    var e = i.getCenter();
-    google.maps.event.trigger(i, "resize"),
-      i.setCenter(e)
-  }),
-    a.setMap(i)
+    var e = map.getCenter();
+    google.maps.event.trigger(map, "resize"),
+      map.setCenter(e)
+  });
+  marker.setMap(map)
 }
