52bb580d4139288e1e14d22e3d81a7c4eed0f4b7
ndgo <durnov.maxim@gmail.com>
2017-06-25 12:59:49 +0300

    fix

--- js/app.js
+++ js/app.js
@@ -1,36 +1,29 @@
 function initMap() {
-  var position = {
-    lat: 59.9387942,
-    lng: 30.3230833
+  var position = new google.maps.LatLng(59.93632276, 30.32106467);
+  var mapParams = {
+    zoom: 16,
+    center: position,
+    disableDefaultUI: !0,
+    zoomControl: !0,
+    zoomControlOptions: {
+      position: google.maps.ControlPosition.LEFT_CENTER
+    },
+    scrollwheel: !1
   };
-  var center = {
-    lat: 59.939794,
-    lng: 30.3230833
-  };
-  var centerParameters = {
-    zoom: 15,
-    center: center
-  };
-  var map = new google.maps.Map(document.getElementById("map"), centerParameters);
-  var icon = {
-    url: "img/icon-map-marker.svg",
-    size: new google.maps.Size(100, 100),
-    scaledSize: new google.maps.Size(35, 35),
-    origin: new google.maps.Point(0, 0),
-    anchor: new google.maps.Point(20, 20),
-    optimized: false,
-    zIndex: 1
-  };
-  var marker = new google.maps.Marker({
-    position: position,
+  map = new google.maps.Map(document.getElementById("map"), mapParams);
+  new google.maps.Marker({
+    position: map.getCenter(),
     map: map,
-    icon: icon,
-    title: "HTML Academy"
+    icon: {
+      path: google.maps.SymbolPath.CIRCLE,
+      scale: 13,
+      fillOpacity: 100,
+      fillColor: "#d22856",
+      strokeWeight: 10,
+      strokeColor: "white"
+    }
   });
   google.maps.event.addDomListener(window, "resize", function () {
-    var e = map.getCenter();
-    google.maps.event.trigger(map, "resize"),
-      map.setCenter(e)
-  });
-  marker.setMap(map)
+    map.setCenter(position)
+  })
 }
